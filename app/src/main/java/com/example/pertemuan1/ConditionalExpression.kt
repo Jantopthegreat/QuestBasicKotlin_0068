66580a5c5691072e6ba96aac0eb1180255de02c8
Jantopthegreat <fauzangriya@gmail.com>
2024-10-14 00:02:49 +0700

    Membuat Percabangan if-else dan membuat variable dengan nilainya, lalu membuat percabangan if-else Jika nomor lebih dari 0 maka print "Positive Number" dan jika selain itu maka print "Negative Number"

--- app/src/main/java/com/example/pertemuan1/ConditionalExpression.kt
+++ app/src/main/java/com/example/pertemuan1/ConditionalExpression.kt
@@ -1,2 +1,18 @@
 package com.example.pertemuan1
 
+// Conditional Expression
+// Conditional expression adalah ekspresi yang mengembalikan nilai berdasarkan kondisi tertentu
+// Conditional expression menggunakan if-else, when, dan try-catch
+
+fun ConditionalStatement() {
+    println("=== Conditional Expression")
+
+    // If-else
+    val number = 10
+    if (number > 0) {
+        println("Positive Number")
+    } else {
+        println("Negative Number")
+    }
+
+}
