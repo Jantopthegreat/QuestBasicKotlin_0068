e073245e4efa89625121dfbc2ad528a4ec396fd3
Jantopthegreat <fauzangriya@gmail.com>
2024-10-14 00:33:14 +0700

    Membuat File LambdaFunction.kt

---
+++ app/src/main/java/com/example/pertemuan1/LambdaFunction.kt
@@ -0,0 +1,6 @@
+package com.example.pertemuan1
+
+// Lambda Functions
+// Lambda Functions adalah fungsi yang tidak memiliki nama
+// Lambda Functions biasanya digunakan untuk membuat fungsi yang sederhana
+// Lambda Functions menggunakan tanda panah -> untuk memisahkan parameter dan body fungsi
