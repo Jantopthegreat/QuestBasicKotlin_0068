9de8ada3cde0d8fb64512ba119cf6278f6b32683
Jantopthegreat <fauzangriya@gmail.com>
2024-10-14 00:29:24 +0700

    Membuat fungsi equals dengan variable data2

--- app/src/main/java/com/example/pertemuan1/DataClass.kt
+++ app/src/main/java/com/example/pertemuan1/DataClass.kt
@@ -14,4 +14,9 @@ data class DataClass(
 fun main() {
     val data = DataClass(1, "indra@gmail.com")
     println(data)
+
+    // Fungsi equals
+    val data2 = DataClass(1, "makan@gmail.com")
+    println(data == data2) // false
+
 }
